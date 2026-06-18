ae161e86f66a47ac6a471f81ed0b6946f21cef67
Anuraj Singh <anuraj.mufc@gmail.com>
2026-06-18 16:56:34 -0400

    feat: switch to PostgreSQL and load DB URL from .env

--- app/database.py
+++ app/database.py
@@ -1,14 +1,19 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.environ["DB_URL"]
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 class Base(DeclarativeBase):
     pass
+
 
 def get_db():
     with SessionLocal() as db:
