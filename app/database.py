9f9c83728370598a58abab17ecdcb9032772fe67
Anuraj Singh <anuraj.mufc@gmail.com>
2026-07-08 12:06:15 -0400

    refactor: load DB_URL form Settings instead of os.environ

--- app/database.py
+++ app/database.py
@@ -1,12 +1,9 @@
-import os
-from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
-load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.environ["DB_URL"]
+from config import settings
 
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_async_engine(settings.DB_URL)
 
 AsyncSessionLocal = async_sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False)
